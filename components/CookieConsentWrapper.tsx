a87d8841792730ba93745a5f2a50a50976b4782b
Ingatlan-Térkép <6almaban@freemail.hu>
2026-06-15 18:20:58 +0200

    naaa?

--- components/CookieConsentWrapper.tsx
+++ components/CookieConsentWrapper.tsx
@@ -1,6 +1,8 @@
+// CookieConsentWrapper.tsx - Teljesen működő verzió
+
 'use client';
 
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import CookieConsent from 'react-cookie-consent';
 import { setCookie, getCookie } from 'cookies-next';
 import Script from 'next/script';
@@ -9,107 +11,161 @@ export default function CookieConsentWrapper() {
   const [mounted, setMounted] = useState(false);
   const [cookiesAccepted, setCookiesAccepted] = useState(false);
 
-  // Inicializálás
+  // Kezdeti állapot betöltése
   useEffect(() => {
     const hasConsent = getCookie('ingatlanTerkepCookieConsent') === 'true';
     setCookiesAccepted(hasConsent);
     setMounted(true);
   }, []);
 
-  // BARION PIXEL - Mindig betöltődik
-  useEffect(() => {
-    if (typeof window === 'undefined' || window.bp) return;
-
-    window.bp = function (...args: any[]) {
-      (window.bp!.q = window.bp!.q || []).push(args);
-    };
-    window.bp.l = Date.now();
-    window.barion_pixel_id = 'BP-YQqhhb7YpN-9B';
-
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = 'https://pixel.barion.com/bp.js';
-    document.head.appendChild(script);
-
-    setTimeout(() => {
-      if (window.bp) {
-        window.bp('init', 'addBarionPixelId', window.barion_pixel_id);
-        console.log('[Barion] Pixel inicializálva');
-      }
-    }, 400);
-  }, []);
-
-  // ==================== ANALYTICS ====================
-  const loadAnalytics = useCallback(() => {
-    if (window.gtag) {
-      console.log('[GA4] Már létezett');
-      return;
-    }
-
-    console.log('[GA4] Betöltés elkezdődött...');
-
-    window.dataLayer = window.dataLayer || [];
-
-    // Biztonságos gtag definíció
-    window.gtag = function (...args: any[]) {
-      console.log('[GA4] gtag hívás:', args);
-      window.dataLayer?.push(args);
-    };
-
-    // GA4 script betöltése
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-KWH607ZP7H';
-    document.head.appendChild(script);
-
-    // Várunk a script betöltődésére
-    setTimeout(() => {
-      if (!window.gtag) return;
-
-      window.gtag('js', new Date());
-      window.gtag('config', 'G-KWH607ZP7H', {
-        send_page_view: true,
-        cookie_flags: 'SameSite=None;Secure'
-      });
-
-      // Teszt event
-      window.gtag('event', 'consent_accepted', {
-        event_category: 'cookie_consent',
-        event_label: 'user_accepted'
-      });
-
-      console.log('[GA4] Config + teszt event elküldve');
-    }, 1200);
-  }, []);
-
-  const handleAccept = useCallback(() => {
+  // Cookie elfogadás kezelése
+  const handleAccept = () => {
     setCookiesAccepted(true);
     setCookie('ingatlanTerkepCookieConsent', 'true', { 
       maxAge: 150 * 24 * 60 * 60, 
       path: '/' 
     });
-
+    
+    // 1. GA4 consent update - type guarddal
+    if (typeof window !== 'undefined' && window.gtag) {
+      window.gtag('consent', 'update', {
+        'analytics_storage': 'granted',
+        'ad_storage': 'granted'
+      });
+      
+      window.gtag('event', 'consent_accepted', {
+        'event_category': 'cookie_consent',
+        'event_label': 'user_accepted'
+      });
+      console.log('[GA4] Consent granted - event sent');
+    } else {
+      console.log('[GA4] gtag nem elérhető még');
+    }
+    
+    // 2. Meta Pixel consent
+    if (typeof window !== 'undefined' && window.fbq) {
+      window.fbq('consent', 'grant');
+      setTimeout(() => {
+        if (window.fbq) {
+          window.fbq('track', 'PageView');
+        }
+      }, 100);
+    }
+    
     window.dispatchEvent(new CustomEvent('cookiesAccepted'));
-    loadAnalytics();
+    console.log('[CookieConsent] Elfogadva');
+  };
 
-    console.log('[CookieConsent] Elfogadva → GA4 betöltése elindítva');
-  }, [loadAnalytics]);
-
-  const handleDecline = useCallback(() => {
+  const handleDecline = () => {
     setCookiesAccepted(false);
     setCookie('ingatlanTerkepCookieConsent', 'false', { 
       maxAge: 150 * 24 * 60 * 60, 
       path: '/' 
     });
+    
+    if (typeof window !== 'undefined' && window.gtag) {
+      window.gtag('consent', 'update', {
+        'analytics_storage': 'denied',
+        'ad_storage': 'denied'
+      });
+    }
+    
     window.dispatchEvent(new CustomEvent('cookiesDeclined'));
     console.log('[CookieConsent] Elutasítva');
-  }, []);
+  };
 
   if (!mounted) return null;
 
   return (
     <>
-      {/* Barion Pixel */}
+      {/* ========== GOOGLE TAG MANAGER & GA4 ========== */}
+      <Script
+        id="gtm-consent"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+            
+            gtag('consent', 'default', {
+              'analytics_storage': 'denied',
+              'ad_storage': 'denied',
+              'wait_for_update': 500
+            });
+            
+            gtag('js', new Date());
+            gtag('config', 'G-KWH607ZP7H', {
+              'send_page_view': false,
+              'cookie_flags': 'SameSite=None;Secure'
+            });
+            
+            console.log('[GA4] Initialized with consent denied');
+          `,
+        }}
+      />
+
+      {/* GTM Script */}
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WVS766GK');
+            console.log('[GTM] Script loaded');
+          `,
+        }}
+      />
+
+      {/* GTM Noscript iframe */}
+      <Script
+        id="gtm-noscript"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            if (typeof document !== 'undefined') {
+              var noscript = document.createElement('noscript');
+              var iframe = document.createElement('iframe');
+              iframe.src = 'https://www.googletagmanager.com/ns.html?id=GTM-WVS766GK';
+              iframe.height = '0';
+              iframe.width = '0';
+              iframe.style.display = 'none';
+              iframe.style.visibility = 'hidden';
+              noscript.appendChild(iframe);
+              document.body.insertBefore(noscript, document.body.firstChild);
+            }
+          `,
+        }}
+      />
+
+      {/* ========== META PIXEL - CSAK HA ELFOGADTÁK ========== */}
+      {cookiesAccepted && (
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1588029335599358');
+              fbq('track', 'PageView');
+              console.log('[Meta Pixel] Loaded and initialized');
+            `,
+          }}
+        />
+      )}
+
+      {/* ========== BARION PIXEL - MINDIG ========== */}
       <Script
         id="barion-pixel"
         strategy="afterInteractive"
@@ -130,28 +186,6 @@ export default function CookieConsentWrapper() {
         strategy="afterInteractive"
         src="https://pixel.barion.com/bp.js"
       />
-
-      {/* Meta Pixel */}
-      {cookiesAccepted && (
-        <Script
-          id="meta-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1588029335599358');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
-      )}
 
       <CookieConsent
         location="bottom"
